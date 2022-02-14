975f7873a2df1a392baf6b6d4cdc5c3d05888710
s01va <jinn0525@gmail.com>
2022-02-14 15:41:37 +0900

    spring boot init

---
+++ src/main/java/com/s01valab/classs01va/Classs01vaApplication.java
@@ -0,0 +1,13 @@
+package com.s01valab.classs01va;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Classs01vaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Classs01vaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/s01valab/classs01va/Classs01vaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s01valab.classs01va;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Classs01vaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
