a6947c0092bf24f6d2b46be8a3b4995382361254
s01va <jinn0525@gmail.com>
2022-02-27 19:03:15 +0900

    all classes show test

--- src/main/java/com/s01valab/classs01va/service/classes/ClassesService.java
+++ src/main/java/com/s01valab/classs01va/service/classes/ClassesService.java
@@ -1,9 +1,22 @@
 package com.s01valab.classs01va.service.classes;
 
+import com.s01valab.classs01va.domain.classes.ClassesRepository;
+import com.s01valab.classs01va.web.dto.ClassesResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class ClassesService {
+    private final ClassesRepository classesRepository;
+
+    public List<ClassesResponseDto> findAll() {
+        return classesRepository.findAll().stream()
+                .map(ClassesResponseDto::new)
+                .collect(Collectors.toList());
+    }
 }
--- src/main/java/com/s01valab/classs01va/web/ClassesApiController.java
+++ src/main/java/com/s01valab/classs01va/web/ClassesApiController.java
@@ -1,4 +1,20 @@
 package com.s01valab.classs01va.web;
 
+import com.s01valab.classs01va.service.classes.ClassesService;
+import com.s01valab.classs01va.web.dto.ClassesResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
 public class ClassesApiController {
+    private final ClassesService classesService;
+
+    @GetMapping("/api/classes")
+    public List<ClassesResponseDto> findAll() {
+        return classesService.findAll();
+    }
 }
